417253f6dad52344005eab0b709ae561e67e2387
cingciong <karmus041@gmail.com>
2023-09-12 15:51:13 +0200

    jmu

---
+++ app/src/main/java/com/example/nekoarc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nekoarc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
